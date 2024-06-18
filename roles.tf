e4ad2c005234b37cfd0af1f023f069b90610ef6c
Vinay Kumar Chelpuri <chelpuri.v@northeastern.edu>
2024-06-17 21:02:31 -0400

    added outputs

--- roles.tf
+++ roles.tf
@@ -1,8 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
 
 # EKS Cluster IAM Role
 resource "aws_iam_role" "eks_cluster" {
