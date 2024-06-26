eb3993e59218b72d54e482b359707f0e18a41a59
Siddharth Dash <dash.sid@northeastern.edu>
2024-06-26 19:06:09 -0400

    feat: adding helm chart deployments

--- helm-charts/kubernetes_ns.tf
+++ helm-charts/kubernetes_ns.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
     config_path = "~/.kube/config"
-    config_context = "arn:aws:eks:us-east-1:654654499596:cluster/csye7125" 
+    config_context = "arn:aws:eks:us-east-1:637423621713:cluster/csye7125" 
 }
 
 resource "kubernetes_namespace" "csye7125" {
